5996ccb4ac75537231b4e22129c2eae94199516d
Essi Monneus <essimonneus@gmail.com>
2021-08-09 09:32:38 -0400

    fix home link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Sitebar = () => {
     return (
         <Navbar color="dark" dark expand="md">
-            <NavbarBrand href="/">
+            <NavbarBrand href="/home">
                 Home
             </NavbarBrand>
             <Nav className="ml-auto">
